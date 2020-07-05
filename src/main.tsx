a5df80848f2076ebd0ac222aec8327c88bf4b2d5
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-07-05 18:42:04 +0900

    chore : add component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {SubComponent} from './sub-component';
+import {SubComponent} from './components/sub-component';
 
 class App extends React.Component {
   render() {
